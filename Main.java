dc0d8ec037df3e1e76064a8e1faa34291895e982
JywagoB&F <luan.jywago@sempreceub.com>
2025-02-22 18:30:00 -0300

    Update add mensages for usuary
    add mensages for usuary

--- Main.java
+++ Main.java
@@ -1,9 +1,16 @@
+import java.util.Locale;
+
 public class Main {
     public static void main(String[] args) {
         String nome = "Gustavo";
         int idade = 18;
+        double renda = 1800.0;
 
-        System.out.printf("O nome da pessoa é %s e essa pessoa possui %d anos de idade||", nome, idade);
+        //ESSE PRINTF VAI FORMATAR A SAÍDA DE DADOS DANDO OS DADOS SEPARADOS DE ENTRADA E,LOGO APOS, COM A ENTRADA DOS ARGUMENTOS UTILIZADOS
+        System.out.printf("O nome da pessoa é %s e essa pessoa possui %d anos de idade, com salário de %.2f reais por mês%n||", nome, idade, renda);
+
+        //O DADO LOCALE VAI LOCALIZAR O PADRÃO US NO SEU COMPUTADOR QUE ESTÁ EM PT-BR
+        Locale.setDefault(Locale.US);
        int a = 9, b = 5 , c;
        if (a >= 5){
         c = a + b;
