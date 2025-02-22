8773e5723f4d71c7ab7867375ad94c306fd05bdc
JywagoB&F <luan.jywago@sempreceub.com>
2025-02-22 18:14:31 -0300

    addition_and_subtraction
    Initial Java projects doing + and -

---
+++ Main.java
@@ -0,0 +1,16 @@
+public class Main {
+    public static void main(String[] args) {
+        String nome = "Gustavo";
+        int idade = 18;
+
+        System.out.printf("O nome da pessoa é %s e essa pessoa possui %d anos de idade||", nome, idade);
+       int a = 9, b = 5 , c;
+       if (a >= 5){
+        c = a + b;
+       }
+       else{
+        c = a - b;
+       }
+       System.out.println(c);
+    }
+}
